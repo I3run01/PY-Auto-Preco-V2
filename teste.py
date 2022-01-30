5aac7cb7d5e16548ceccc287a57b22c0f7caea73
I3run01 <brunnooa.v@gmail.com>
2022-01-30 08:55:59 -0300

    commit

--- teste.py
+++ teste.py
@@ -2,12 +2,12 @@ import pandas as pd
 import InLondrisoft as il
 
 
-df = pd.read_csv('Bocchi BD.csv')
+df = pd.read_csv('Arcom BD.csv')
 
 lcodigos = []
 lprecos = []
 
-for c in range(140,len(df)):
+for c in range(16,len(df)):
     lcodigos.append(df.iloc[c][2])
     preco = str(df.iloc[c][6])
     preco = preco.replace('.',',')
