ea1cb7e1a9a2d7d5643d18a74c32c40193f3f610
I3run01 <brunnooa.v@gmail.com>
2022-07-06 11:46:50 -0300

    c

--- ProgPrincipal.py
+++ ProgPrincipal.py
@@ -139,7 +139,7 @@ def bibemp(num):
         nome = 'Oliveira'
         nomecsv = 'Oliveira BD.csv'
     
-    if num == '77595395000490':
+    if num == '77':
         nome = 'Frimesa'
         nomecsv = 'Frimesa BD.csv'
 
