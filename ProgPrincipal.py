fa1fbc4232652d2a84d3167acfd143437a8df612
I3run01 <brunnooa.v@gmail.com>
2022-11-20 09:04:22 -0300

    c

--- ProgPrincipal.py
+++ ProgPrincipal.py
@@ -17,151 +17,151 @@ def cnpj(xml):
 def bibemp(num):
     if num == '62461140002834':
         nome = 'Panco'
-        nomecsv ='Panco BD.csv'
+        nomecsv ='PBanco de dados/anco BD.csv'
         
     elif num =='61186888014305':
         nome ='Coca-Cola'
-        nomecsv = 'CC BD.csv'
+        nomecsv = 'Banco de dados/CC BD.csv'
 
     elif num =='24782106000152':
         nome ='Tirol'
-        nomecsv = 'Tirol BD.csv'
+        nomecsv = 'Banco de dados/Tirol BD.csv'
 
     elif num == '77887412000543':
         nome = 'DEYCON'
-        nomecsv = 'Deycon BD.csv'
+        nomecsv = 'Banco de dados/Deycon BD.csv'
 
     elif num == '12133164000176':
         nome = 'RD'
-        nomecsv = 'RD BD.csv'
+        nomecsv = 'Banco de dados/RD BD.csv'
 
     elif num == '93209765047342':
         nome = 'WMS'
-        nomecsv = 'WMS BD.csv'
+        nomecsv = 'Banco de dados/WMS BD.csv'
 
     elif num == '03303285000128':
         nome = 'Arrojito'
-        nomecsv = 'Arrojito BD.csv'
+        nomecsv = 'Banco de dados/Arrojito BD.csv'
 
     elif num == '73778144000147':
         nome = 'Triunfante'
-        nomecsv = 'Triunfante BD.csv'
+        nomecsv = 'Banco de dados/Triunfante BD.csv'
 
     elif num == '01333984000438':
         nome = 'Segalas'
-        nomecsv = 'Segalas BD.csv'
+        nomecsv = 'Banco de dados/Segalas BD.csv'
 
     elif num == '13495487000253':
         nome = 'Destro'
-        nomecsv = 'Destro BD.csv'
+        nomecsv = 'Banco de dados/Destro BD.csv'
 
     elif num == '16624530000140':
         nome = 'BOCCHI'
-        nomecsv = 'Bocchi BD.csv'
+        nomecsv = 'Banco de dados/Bocchi BD.csv'
 
     elif num == '07757548000120':
         nome = 'CBN'
-        nomecsv = 'CBN BD.csv'
+        nomecsv = 'Banco de dados/CBN BD.csv'
 
     elif num == '11330623000149':
         nome = 'STAMPA'
-        nomecsv = 'Stampa BD.csv'
+        nomecsv = 'Banco de dados/Stampa BD.csv'
 
     elif num == '56228356005958':
         nome = 'Ambev'
-        nomecsv = 'Ambev BD.csv'
+        nomecsv = 'Banco de dados/Ambev BD.csv'
 
     elif num == '21381251000133':
         nome = 'Vigor'
-        nomecsv = 'Vigor BD.csv'
+        nomecsv = 'Banco de dados/Vigor BD.csv'
 
     elif num == '76492305000200':
         nome = 'Ouro fino'
-        nomecsv = 'Ouro fino BD.csv'
+        nomecsv = 'Banco de dados/Ouro fino BD.csv'
 
     elif num == '60409075019767':
         nome = 'Nestle'
-        nomecsv = 'Nestle BD.csv'
+        nomecsv = 'Banco de dados/Nestle BD.csv'
 
     elif num == '28842819000115':
         nome = 'BDL'
-        nomecsv = 'BDL BD.csv'
+        nomecsv = 'Banco de dados/BDL BD.csv'
 
     elif num == '09241957000102':
         nome = 'Amabile'
-        nomecsv = 'Amabile BD.csv'
+        nomecsv = 'Banco de dados/Amabile BD.csv'
 
     elif num == '25769266002844':
         nome = 'Arcom'
-        nomecsv = 'Arcom BD.csv'
+        nomecsv = 'Banco de dados/Arcom BD.csv'
 
     elif num == '15814440000150':
         nome = 'IBD'
-        nomecsv = 'IBD BD.csv'
+        nomecsv = 'Banco de dados/IBD BD.csv'
 
     elif num == '81611931000209':
         nome = 'OESA'
-        nomecsv = 'OESA BD.csv'
+        nomecsv = 'Banco de dados/OESA BD.csv'
 
     elif num == '01838723012567':
         nome = 'BRF'
-        nomecsv = 'BRF BD.csv'
+        nomecsv = 'Banco de dados/BRF BD.csv'
 
     elif num == '09095640000105':
         nome = 'Sueko'
-        nomecsv = 'Sueko BD.csv'
+        nomecsv = 'Banco de dados/Sueko BD.csv'
 
     elif num == '17790307000640':
         nome = 'E-UB'
-        nomecsv = 'E-UB BD.csv'
+        nomecsv = 'Banco de dados/E-UB BD.csv'
 
     elif num == '90724261000902':
         nome = 'Oniz'
-        nomecsv = 'Oniz BD.csv'
+        nomecsv = 'Banco de dados/Oniz BD.csv'
 
     elif num == '00249906000144':
         nome = 'Lemes e Oliveiras'
-        nomecsv = 'Lemes e Oliveiras BD.csv'
+        nomecsv = 'Banco de dados/Lemes e Oliveiras BD.csv'
 
     elif num == '19195971000162':
         nome = 'DP4'
-        nomecsv = 'DP4 BD.csv'
+        nomecsv = 'Banco de dados/DP4 BD.csv'
 
     elif num == '01554188000182':
         nome = 'Grupo Lobo'
-        nomecsv = 'Grupo Lobo BD.csv'
+        nomecsv = 'Banco de dados/Grupo Lobo BD.csv'
 
     elif num == '10842044000384':
         nome = 'Copini'
-        nomecsv = 'Copini BD.csv'
+        nomecsv = 'Banco de dados/Copini BD.csv'
 
     elif num == '24509214000156':
         nome = 'Oliveira'
-        nomecsv = 'Oliveira BD.csv'
+        nomecsv = 'Banco de dados/Oliveira BD.csv'
 
     elif num == '77595395000490':
         nome = 'Frimesa'
-        nomecsv = 'Frimesa BD.csv'
+        nomecsv = 'Banco de dados/Frimesa BD.csv'
     
     elif num == '29635803000102':
         nome = 'Hawaii Distribuição de materiais'
-        nomecsv = 'Hawaii BD.csv'
+        nomecsv = 'Banco de dados/Hawaii BD.csv'
 
     elif num == '76430438010134':
         nome = 'Max Atacadista'
-        nomecsv = 'Max BD.csv'
+        nomecsv = 'Banco de dados/Max BD.csv'
 
     elif num == '36486464000105':
         nome = 'Chef Distribuidora'
-        nomecsv = 'Chef BD.csv'
+        nomecsv = 'Banco de dados/Chef BD.csv'
 
     elif num == '17467515002738':
         nome = 'Cafe Tres Coracoes S.A'
-        nomecsv = 'Três corações BD.csv'
+        nomecsv = 'Banco de dados/Três corações BD.csv'
 
     elif num == '02914460045170':
         nome = 'Seara'
-        nomecsv = 'Seara BD.csv'
+        nomecsv = 'Banco de dados/Seara BD.csv'
     
     else:
         print('Empresa não cadastrada')
